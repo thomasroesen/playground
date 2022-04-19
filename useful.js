b0cd2f1d8b5b52a6f8e914f7a60e4cd9a8d09659
thomasroesen <thomasr@enkelt.no>
2022-04-19 14:21:28 +0200

    div

---
+++ useful.js
@@ -0,0 +1,42 @@
+// Shuffle array
+const shuffle = (arr) => arr.sort(() => Math.random() - 0.5)
+const arr = [1,2,3,4,5,6,7,8,9,10]
+console.log(shuffle(arr))
+
+// Copy to clipboard
+const copyToClipBoard = (text) => navigator.clipboard?.writeText && 
+navigator.clipboard.writeText(text)
+copyToClipBoard('copied son')
+
+// Get unique elements
+const getUnique = (arr) => [... new Set(arr)]
+const array = [1, 1, 2, 3, 3, 4, 4, 4, 5, 5]
+console.log(getUnique(array))
+
+// Detect dark mode
+const isDarkMode = () =>
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+console.log(isDarkMode());
+
+// Scroll to top
+const scrollToTop = (element) =>
+  element.scrollIntoView({ behavior: "smooth", block: "start" });
+
+// Scroll to bottom
+const scrollToBottom = (element) =>
+  element.scrollIntoView({ behavior: "smooth", block: "end" });
+
+
+// Generate random color
+const generateRandomHexColor = () =>
+  `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
+
+
+// Websites
+https://devdocs.io/ //  numerous API documentation in a single, searchable interface.
+https://ray.so/ // Code screenshots
+https://codebeautify.org/ // Code beautifier - JSON viewer, XML to json, image to base64 etc etc
+https://colorhunt.co/ // Color schemes
+https://readme.so/editor // Md editor
+
